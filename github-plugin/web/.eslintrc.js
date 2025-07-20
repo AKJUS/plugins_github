5494f929be6d9b0b54ac40a44d8c956bf4757306
Paladox none <thomasmulhall410@yahoo.com>
2025-07-20 06:44:37 -0700

    Migrate .eslintrc.js to eslint.config.js
    Change-Id: I49980b6d14a45846630cdc99e208086ac84626ae

--- github-plugin/web/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-/**
- * @license
- * Copyright 2022 Google LLC
- * SPDX-License-Identifier: Apache-2.0
- */
-__plugindir = "github/github-plugin/web";
-module.exports = {
-  extends: "../../../.eslintrc.js",
-};
