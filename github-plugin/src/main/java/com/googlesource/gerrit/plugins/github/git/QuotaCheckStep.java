GerritForge/github

Honor repo size quota enforcer during project import
Imports from GitHub did not consult repository-size quota enforcers,
allowing projects that exceeded configured limits to proceed, leading to
quota violations.

Introduce a preflight quota check to enforce limits early:
* Add `QuotaCheckStep`, which fetches the GitHub repository size and
  performs a `QuotaBackend` dry-run against `REPOSITORY_SIZE_GROUP`,
failing on error.
* Wire the step via Guice and `GitImporter`, placing it before clone,
  project creation so we fail fast with a clear error.
* Add `QuotaEnforcedException` for consistent, user-friendly error
  reporting when quota blocks an import.

When quotas allow, the import flow is unchanged. This ensures project
imports respect site policies rather than exceeding quota size limits.

Bug: Issue 445826646
Change-Id: I2ad8d35fac57581fc5e8fbc25134e0fdb90f52ab

github-plugin/src/main/java/com/googlesource/gerrit/plugins/github/git/QuotaCheckStep.java
@@ -1,0 +1,78 @@
+// Copyright (C) 2025 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package com.googlesource.gerrit.plugins.github.git;
+
+import static com.google.gerrit.server.quota.QuotaGroupDefinitions.REPOSITORY_SIZE_GROUP;
+
+import com.google.gerrit.entities.Project;
+import com.google.gerrit.server.quota.QuotaBackend;
+import com.google.gerrit.server.quota.QuotaResponse;
+import com.google.inject.Inject;
+import com.google.inject.assistedinject.Assisted;
+import com.googlesource.gerrit.plugins.github.GitHubConfig;
+import org.eclipse.jgit.lib.ProgressMonitor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class QuotaCheckStep extends ImportStep {
+  private static final Logger LOG = LoggerFactory.getLogger(QuotaCheckStep.class);
+  private final QuotaBackend quotaBackend;
+
+  public interface Factory {
+    QuotaCheckStep create(
+        @Assisted("organisation") String organisation, @Assisted("name") String repository);
+  }
+
+  @Inject
+  public QuotaCheckStep(
+      QuotaBackend quotaBackend,
+      GitHubConfig config,
+      GitHubRepository.Factory gitHubRepoFactory,
+      @Assisted("organisation") String organisation,
+      @Assisted("name") String repository) {
+    super(config.gitHubUrl, organisation, repository, gitHubRepoFactory);
+    this.quotaBackend = quotaBackend;
+  }
+
+  @Override
+  public void doImport(ProgressMonitor progress) throws GitException {
+    Project.NameKey fullProjectName =
+        Project.nameKey(getOrganisation() + "/" + getRepositoryName());
+    try {
+      progress.beginTask("Getting repository size", 1);
+      LOG.info("{}|Getting repository size", fullProjectName);
+      int size = getRepository().getSize() * 1024;
+      LOG.info("{}|Repository size: {} Kb", fullProjectName, size);
+
+      LOG.info("{}|Checking repository size is allowed by quota", fullProjectName);
+      if (size > 0) {
+        QuotaResponse.Aggregated aggregated =
+            quotaBackend.currentUser().project(fullProjectName).dryRun(REPOSITORY_SIZE_GROUP, size);
+        aggregated.throwOnError();
+      }
+      progress.update(1);
+    } catch (Exception e) {
+      LOG.error("{}|Quota does not allow importing repo", fullProjectName, e);
+      throw new GitException(e.getMessage(), e);
+    } finally {
+      progress.endTask();
+    }
+    LOG.info("{}|SUCCESS repository size is allowed", fullProjectName);
+  }
+
+  @Override
+  public boolean rollback() {
+    return true;
+  }
+}

AKJUS/plugins_github

github-plugin/src/main/java/com/googlesource/gerrit/plugins/github/git/QuotaCheckStep.java
@@ -1,0 +1,80 @@
+// Copyright (C) 2025 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package com.googlesource.gerrit.plugins.github.git;
+
+import static com.google.gerrit.server.quota.QuotaGroupDefinitions.REPOSITORY_SIZE_GROUP;
+
+import com.google.gerrit.entities.Project;
+import com.google.gerrit.server.quota.QuotaBackend;
+import com.google.gerrit.server.quota.QuotaResponse;
+import com.google.inject.Inject;
+import com.google.inject.assistedinject.Assisted;
+import com.googlesource.gerrit.plugins.github.GitHubConfig;
+import org.eclipse.jgit.lib.ProgressMonitor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class QuotaCheckStep extends ImportStep {
+  private static final Logger LOG = LoggerFactory.getLogger(QuotaCheckStep.class);
+  private final QuotaBackend quotaBackend;
+
+  private static final long BYTES_PER_KB = 1024L;
+
+  public interface Factory {
+    QuotaCheckStep create(
+        @Assisted("organisation") String organisation, @Assisted("repository") String repository);
+  }
+
+  @Inject
+  public QuotaCheckStep(
+      QuotaBackend quotaBackend,
+      GitHubConfig config,
+      GitHubRepository.Factory gitHubRepoFactory,
+      @Assisted("organisation") String organisation,
+      @Assisted("repository") String repository) {
+    super(config.gitHubUrl, organisation, repository, gitHubRepoFactory);
+    this.quotaBackend = quotaBackend;
+  }
+
+  @Override
+  public void doImport(ProgressMonitor progress) throws GitException {
+    Project.NameKey fullProjectName =
+        Project.nameKey(getOrganisation() + "/" + getRepositoryName());
+    try {
+      progress.beginTask("Getting repository size", 1);
+      LOG.debug("{}|Getting repository size", fullProjectName);
+      long size = getRepository().getSize() * BYTES_PER_KB;
+      LOG.debug("{}|Repository size: {} Kb", fullProjectName, size);
+
+      LOG.debug("{}|Checking repository size is allowed by quota", fullProjectName);
+      if (size > 0) {
+        QuotaResponse.Aggregated aggregated =
+            quotaBackend.currentUser().project(fullProjectName).dryRun(REPOSITORY_SIZE_GROUP, size);
+        aggregated.throwOnError();
+      }
+      progress.update(1);
+    } catch (Exception e) {
+      LOG.error("{}|Quota does not allow importing repo", fullProjectName, e);
+      throw new QuotaEnforcedException(e.getMessage(), e);
+    } finally {
+      progress.endTask();
+    }
+    LOG.debug("{}|SUCCESS repository size is allowed", fullProjectName);
+  }
+
+  @Override
+  public boolean rollback() {
+    return true;
+  }
+}